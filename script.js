4cf0adcf6e9408ad468c6611f522f0427c61793c
ayarmkt <ayat.0906@gmail.com>
2021-07-03 18:12:00 +0900

    updated JS

--- script.js
+++ script.js
@@ -15,8 +15,25 @@ let firstNum = '';
 let secondNum = '';
 let sum = '';
 
+/////HELPER FUNCTION/////
+//
+
+const displayInProcess = function (key) {
+  process.textContent = `${firstNum} ${key.textContent}`;
+};
+
+const resetVariables = function () {
+  secondNum = '';
+  sum = '';
+};
+
+const emptyResult = function () {
+  result.textContent = '';
+};
+
 /////FUNCTION/////
 //
+
 const calculate = function (key, firstNum) {
   secondNum = result.textContent;
   switch (key && key.dataset.action) {
@@ -35,32 +52,41 @@ const calculate = function (key, firstNum) {
   }
 };
 
-const displayInProcess = function (key) {
-  process.textContent = `${firstNum} ${key.textContent}`;
-};
-
 const handleOperator = function (key, prevKey) {
   if (prevKey && prevKey.matches('.operator')) {
-    displayInProcess(key);
+    if (prevKey.dataset.action === 'calculate' && result.textContent !== '0') {
+      console.log('NEED TO SUM, AND MAKE THE SUM THE FIRST');
+      calculate(prevOperator, firstNum);
+      firstNum = result.textContent;
+      displayInProcess(key);
+      resetVariables();
+      emptyResult();
+    } else if (process.textContent) {
+      console.log('CHANGE OPERATOR');
+      displayInProcess(key);
+    } else {
+      console.log('error 2');
+    }
   } else if (firstNum) {
     console.log('NEED TO SUM, AND MAKE THE SUM THE FIRST');
-    console.log(firstNum, secondNum);
     calculate(prevOperator, firstNum);
     firstNum = sum;
-    secondNum = '';
-    sum = '';
     displayInProcess(key);
+    resetVariables();
     result.textContent = '';
-  } else if ((!firstNum && prevKey) || prevKey.dataset.action === 'calculate') {
-    //when first key is operator
-    //when you type only operators or funct
-    //after equal, the result number becomes the first number and calculates again
+  } else if (
+    !firstNum &&
+    prevKey &&
+    (key.dataset.action === 'subtract' || key.dataset.action === 'add')
+  ) {
     console.log('FIRST NUMBER TYPED, WAITING FOR SECOND NUMBER');
-    console.log(firstNum, secondNum);
     firstNum = result.textContent;
     displayInProcess(key);
-    result.textContent = '';
-  } else if (!prevKey) {
+    emptyResult();
+  } else if (
+    key.dataset.action === 'divide' ||
+    key.dataset.action === 'multiply'
+  ) {
     console.log('NEED TO TYPE NUMBER');
   } else {
     console.log('error');
@@ -131,13 +157,12 @@ buttons.addEventListener('click', function (e) {
       console.log(`its equal`);
       calculate(prevOperator, firstNum);
       console.log(firstNum);
-      result.textContent = sum;
+      console.log(sum);
+      result.textContent = sum === '' ? '0' : sum;
       firstNum = result.textContent;
       process.textContent = '';
       sum = '';
       //also want to change the operator color style back to default
-
-      calculate(key, firstNum);
       break;
 
     case 'clear':
